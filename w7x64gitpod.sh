e276294985ed8a02758060c4cf88fbaf181211d0
44521 <132721007+44521@users.noreply.github.com>
2023-08-08 22:15:04 +0800

    Update w7x64gitpod.sh

--- w7x64gitpod.sh
+++ w7x64gitpod.sh
@@ -1,6 +1,6 @@
 apt-get update
 echo "Download windows files"
-wget -O w7x64.img https://bit.ly/akuhnetw7X64
+wget -O w7x64.img https://drive.google.com/file/d/1JCdgCoc7zRXIxowlpiPTJqFpKtRAQ2K2/view?usp=sharing
 echo "Download ngrok"
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
